6d08615d960fd8671e8b24babdcc497fe3879499
a.movchan <a.movchan@elfsight.com>
2023-02-05 17:49:22 +0300

    lesson 12

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,17 @@
-import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPageAsync as AboutPage } from "pages/AboutPage";
-import { MainPageAsync as MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 
 export const App = () => {
 	const { theme, toggleTheme } = useTheme();
 	return (
 		<div className={classNames('app', {}, [theme])}>
+			<Navbar />
+			<AppRouter />
 			<button onClick={toggleTheme}>Toggle</button>
-			<Link to={"/"}>Главная</Link>
-			<Link to={"/about"}>О сайте</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route path="/about" element={<AboutPage />} />
-					<Route path="/" element={<MainPage />} />
-				</Routes></Suspense>
 		</div>
 	);
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,10 +1,10 @@
 // Record своего рода связь ключ - значение
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
 	return [
 		cls,
-		...additional,
+		...additional.filter(Boolean),
 		...Object.entries(mods).filter(([_, value]) => !!value).map(([className]) => className),
 	].join(' ');
 }
